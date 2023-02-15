0339d605e63f18302758ff3c50dc619532e7c46f
r12esh <ritesh098765432@gmail.com>
2023-02-15 14:16:28 +0530

    Include required cdns and script tags

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import AppContextProvider from "./app/shared/context/AppContextProvider";
-import jquery from 'jquery'
-window.$ = window.jQuery = jquery
-// import function from "../src/assets/js/functions";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
