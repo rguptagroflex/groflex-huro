2e4fdfcf25cf61beecec376fc6996ae2b10904d3
Nikhilesh Jadhav <nikhileshjadhav@Nikhileshs-MacBook-Air.local>
2023-06-15 11:46:20 +0530

    add user data api call

--- src/app/redux/reducers/accountDataReducer.js
+++ src/app/redux/reducers/accountDataReducer.js
@@ -2,12 +2,15 @@ import * as actionTypes from "../actions/actions.types";
 
 const initialState = {
   tenantData: undefined,
+  userData: undefined,
 };
 
 export function accountDataReducer(state = initialState, action) {
   switch (action.type) {
     case actionTypes.SET_TENANT_DATA:
       return { ...state, tenantData: action.payload };
+    case actionTypes.SET_USER_DATA:
+      return { ...state, userData: action.payload };
     default:
       return state;
   }
