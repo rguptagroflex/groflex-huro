c550d5acad057fcc2da3d6d90caf1919df7839cc
Keshav-groflex <ksehgal.groflex.io>
2024-02-08 15:11:29 +0530

    dashboard expense by article and payee created, api not working

--- src/app/views/dashboard/DashboardExpenseByPayee.jsx
+++ src/app/views/dashboard/DashboardExpenseByPayee.jsx
@@ -1,141 +1,137 @@
 import moment from "moment";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { SelectInput } from "../../shared/components/select/SelectInput";
 import { FeatherIcon } from "../../shared/featherIcon/FeatherIcon";
-
+import DashboardChartCard from "./DashboardChartCard";
+import groflexService from "../../services/groflex.service";
+import config from "../../../../config";
 const DashboardExpenseByPayee = () => {
   const [date, setDate] = useState({
     startDate: "",
     endDate: "",
   });
-
   const [chartType, setChartType] = useState(true);
+  const [filter, setFilter] = useState("filterByName");
+  const [response, setResponse] = useState(null);
+  const [labels, setLabels] = useState([]);
+  const [series, setSeries] = useState([]);
+  const [entries, setEntries] = useState([]);
 
-  const handleDateDropDown = (option) => {
-    setDate(option.value);
+  const fetchSalesByArticles = () => {
+    let labels = [];
+    let series = [];
+    let entries = [];
+    groflexService
+      .request(
+        `${config.resourceUrls.salesByArticles(date.startDate, date.endDate)}`,
+        { auth: true }
+      )
+      .then((res) => {
+        setResponse(res.body.data);
+      });
   };
 
-  const dateOptions = [
+  useEffect(() => {
+    fetchSalesByArticles();
+  }, [date]);
+
+  useEffect(() => {
+    if (response) {
+      createChartData(filter);
+    }
+  }, [response]);
+
+  const handleFilterChange = (option) => {
+    setFilter(option.value);
+    createChartData(option.value);
+  };
+
+  const createChartData = (value) => {
+    let labels = [];
+    let series = [];
+    let entries = [];
+    const colors = [
+      "rgb(251, 195, 177)",
+      "rgb(247, 140, 107)",
+      "rgb(245, 104, 61)",
+      "rgb(233, 64, 12)",
+      "rgb(194, 53, 10)",
+    ];
+    // if (response && response?.articles) {
+    if (value === "filterByName") {
+      response.articles.custom.forEach((article, id) => {
+        labels.push(article.name);
+        series.push({ value: article.value, className: `pie-${id}` });
+        // series.push(article.value);
+        entries.push({
+          label: article.name,
+          value: article.value,
+          color: colors[id],
+        });
+      });
+    }
+
+    if (value === "filterByCategory") {
+      response.articleCategories.custom.forEach((category, id) => {
+        labels.push(category.name);
+        series.push({ value: category.value, className: `pie-${id}` });
+        entries.push({
+          label: category.name,
+          value: category.value,
+          color: colors[id],
+        });
+      });
+    }
+    // }
+
+    setLabels(labels);
+    setSeries(series);
+    setEntries(entries);
+  };
+
+  const chartData = {
+    labels: labels,
+    series: chartType ? [series] : series,
+  };
+
+  const chartOptions = chartType
+    ? {
+        width: "400px",
+        height: "300px",
+      }
+    : {
+        width: "400px",
+        height: "300px",
+        donut: true,
+        donutWidth: 60,
+        startAngle: 270,
+        showLabel: true,
+      };
+  const filterOptions = [
     {
-      label: moment().format("MMMM"),
-      value: {
-        startDate: moment().startOf("month").format(),
-        endDate: moment().endOf("month").format(),
-      },
+      label: "Filter By Category",
+      value: "filterByCategory",
     },
     {
-      label: moment().subtract(1, "months").format("MMMM"),
-      value: {
-        startDate: moment().subtract(1, "months").startOf("month").format(),
-        endDate: moment().subtract(1, "months").endOf("month").format(),
-      },
-    },
-    {
-      label: moment().subtract(2, "months").format("MMMM"),
-      value: {
-        startDate: moment().subtract(2, "months").startOf("month").format(),
-        endDate: moment().subtract(2, "months").endOf("month").format(),
-      },
-    },
-    {
-      label: `Quarter ${moment().startOf("quarter").format("Q/YYYY")}`,
-      value: {
-        startDate: moment().startOf("quarter").format(),
-        endDate: moment().endOf("quarter").format(),
-      },
-    },
-    {
-      label: `Quarter ${moment()
-        .subtract(3, "months")
-        .startOf("quarter")
-        .format("Q/YYYY")}`,
-      value: {
-        startDate: moment().subtract(3, "months").startOf("quarter").format(),
-        endDate: moment()
-          .subtract(3, "months")
-          .endOf("quarter")
-          .format("DD MMMM YYYY"),
-      },
-    },
-    {
-      label: `Quarter ${moment()
-        .subtract(6, "months")
-        .startOf("quarter")
-        .format("Q/YYYY")}`,
-      value: {
-        startDate: moment().subtract(6, "months").startOf("quarter").format(),
-        endDate: moment().subtract(6, "months").endOf("quarter").format(),
-      },
+      label: "Filter By Name",
+      value: "filterByName",
     },
   ];
-  console.log(date);
   return (
-    <div className="dashboard-invoice-tab-wrapper">
-      <div className="columns is-multiline invoice-tab-header">
-        <div className="column is-6">
-          <SelectInput
-            options={dateOptions}
-            placeholder={"None"}
-            onChange={handleDateDropDown}
-            value={date}
-          />
-        </div>
-        <div
-          className="column is-1 toggle-chart-btn"
-          onClick={() => setChartType(!chartType)}
-        >
-          <FeatherIcon
-            name={chartType ? "PieChart" : "BarChart"}
-            size={20}
-            color="rgb(17, 138, 178)"
-          />
-        </div>
-      </div>
-
-      <div className="column is-12 donut-chart-wrapper"></div>
-      <div className="columns is-multiline value-categories">
-        <div className="column is-6">
-          <span
-            className="value-category-dot"
-            style={{ backgroundColor: "rgb(251, 195, 177)" }}
-          ></span>
-          <div>
-            {" "}
-            <p className="value-category-text">
-              Payee 1 {`(2)`}{" "}
-              <FeatherIcon
-                name={"ArrowUpRight"}
-                size={20}
-                color="rgb(17, 138, 178)"
-              />
-            </p>
-            <p className="value-category-value">
-              ₹ 84 <span>{"| "} 100 %</span>
-            </p>
-          </div>
-        </div>
-        <div className="column is-6">
-          <span
-            className="value-category-dot"
-            style={{ backgroundColor: "rgb(247, 140, 107" }}
-          ></span>
-          <div>
-            {" "}
-            <p className="value-category-text">
-              Payye 2 {`(2)`}{" "}
-              <FeatherIcon
-                name={"ArrowUpRight"}
-                size={20}
-                color="rgb(17, 138, 178)"
-              />
-            </p>
-            <p className="value-category-value">
-              ₹ 84 <span>{"| "} 100 %</span>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <DashboardChartCard
+      className={"dashboard-expense-by-tab-wrapper"}
+      headerClassName={"expense-by-tab-header"}
+      chartData={chartData}
+      chartOptions={chartOptions}
+      chartId={"expenseByPayee"}
+      chartType={chartType}
+      setChartType={setChartType}
+      chartEntries={entries}
+      setDate={setDate}
+      filterOptions={filterOptions}
+      handleFilterChange={handleFilterChange}
+      filter={filter}
+    />
   );
 };
 
